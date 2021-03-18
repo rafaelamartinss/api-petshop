40ba9a3d7f7d50d0b78c164ec47dafcd73b64570
Rafaela Leal <rafaelalealmartinss@gmail.com>
2021-03-18 10:30:40 -0300

    add result model

--- src/backoffice/controllers/customer.controller.ts
+++ src/backoffice/controllers/customer.controller.ts
@@ -1,35 +1,31 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
 import { Customer } from "../models/customer.model";
+import { Result } from "../models/result.model";
 
 @Controller('customer')
 export class CustomerController {
     @Get()
     findAll() {
-        return "All clients";
+        return new Result(null, true, [], null);
     }
 
     @Get(':document')
     findOne(@Param('document') document: string) {
-        return 'Client:' + document;
+        return new Result(null, true, {}, null);
     }
 
     @Post()
     create(@Body() body: Customer) {
-        return body;
+        return new Result('Customer created!', true, body, null);
     }
 
     @Put(':document')
     update(@Param('document') document, @Body() body: Customer) {
-        return {
-            customer: document,
-            data: body
-        };
+        return new Result('Customer updated!', true, body, null);
     }
 
     @Delete(':document')
     delete(@Param('document') document) {
-        return {
-            customer: document
-        }
+        return new Result('Customer deleted!', true, null, null);
     }
 }
