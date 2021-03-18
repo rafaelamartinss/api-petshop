d3132cdcba6cfb394ccc70545b86f7c3dd47ddff
Rafaela Leal <rafaelalealmartinss@gmail.com>
2021-03-18 10:42:58 -0300

    add models for customer details

---
+++ src/backoffice/models/pet.model.ts
@@ -0,0 +1,10 @@
+export class Pet {
+    constructor(
+        public name: string,
+        public gender: string,
+        public specie: string,
+        public brand: string
+    ) {
+        
+    }
+}
